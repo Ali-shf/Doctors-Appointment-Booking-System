6863e081a472dfbf90288bfa259407367d008b03
Amirmahdi Golahmar <amirmahdigolahmar@gmail.com>
2025-09-15 13:44:01 +0330

    add extra user field [gender, updated_at, national_code, country, province, city, address] + add UserAdmin class

--- Doctors_Appointment_Booking_System/account/admin.py
+++ Doctors_Appointment_Booking_System/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        ("Extra Info", {"fields": ("gender", "national_code", "updated_at")}),
+    )
--- Doctors_Appointment_Booking_System/account/models.py
+++ Doctors_Appointment_Booking_System/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    GENDER_CHOICES = [
+        ("M", "Male"),
+        ("F", "Female")
+        ("O", "Other"),
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+    updated_at = models.DateTimeField()
+    national_code = models.CharField(max_length=10, blank=True, null=True)
+
