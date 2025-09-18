166d74d4e8c46cec91a8ab1f1ed9eb9ba333eba9
Ali Shahrabi <166712945+Ali-shf@users.noreply.github.com>
2025-09-18 14:08:30 +0330

    Revert "Feat wallet"

--- Doctors_Appointment_Booking_System/reservation/admin.py
+++ Doctors_Appointment_Booking_System/reservation/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
-from .models import Timesheet,Visit,EmailMessage
 
-@admin.register(Timesheet)
-class TimesheetAdmin(admin.ModelAdmin):
-    
-    list_display = ("doctor","clinic","end")
-    search_fields = ("doctor__user__username", "clinic__name")
-    
-@admin.register(Visit)
-class VisitAdmin(admin.ModelAdmin):
-    list_display = ("doctor", "patient", "clinic", "date", "start_meet", "price")
-    list_filter = ("clinic", "doctor")  
-    search_fields = ("doctor__user__username", "patient__username")  
-
-@admin.register(EmailMessage)
-class EmailMessageAdmin(admin.ModelAdmin):
-    list_display = ("visit", "description")
-    search_fields = ("visit__doctor__user__username", "visit__patient__username")
+# Register your models here.
--- Doctors_Appointment_Booking_System/wallet/admin.py
+++ Doctors_Appointment_Booking_System/wallet/admin.py
@@ -1,20 +1,3 @@
 from django.contrib import admin
 
-from .models import Wallet, Cart, Transaction
-
-@admin.register(Wallet)
-class WalletAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'code', 'balance', 'created_at', 'updated_at')
-    search_fields = ('user__username', 'code')
-    list_filter = ('created_at', 'updated_at')
-@admin.register(Cart)
-class CartAdmin(admin.ModelAdmin):
-    list_display = ('id', 'pay_price', 'payment_status', 'support_code', 'created_at', 'updated_at')
-    search_fields = ('support_code', )
-    list_filter = ('payment_status', 'created_at', 'updated_at')
-
-@admin.register(Transaction)
-class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'wallet', 'payment_type', 'amount', 'cart', 'created_at')
-    search_fields = ('wallet__user__username', 'cart__id')
-    list_filter = ('payment_type', 'created_at')
+# Register your models here.
